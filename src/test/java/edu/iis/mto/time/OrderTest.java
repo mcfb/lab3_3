a4cb80e6f32bd13b46f9b509cfc30b5c44475a8a
Fabicki.Maciej <209305@edu.p.lodz.pl>
2019-04-25 22:26:44 +0200

    test shouldBeSumbittedIfOrderIsNotExpired and add setup

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,19 +1,43 @@
 package edu.iis.mto.time;
 
 import com.sun.org.apache.xpath.internal.operations.Or;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class OrderTest {
 
+
+    private FakeDateTime fakeDateTime;
+    private Order order;
+
+    private void setup(){
+         fakeDateTime = new FakeDateTime();
+         order = new Order(fakeDateTime);
+    }
+
+
+
     @Test(expected = OrderExpiredException.class)
     public void shouldThrowOrderExpiredIfOrderIsExpired() {
-        FakeDateTime fakeDateTime = new FakeDateTime();
-        Order order = new Order(fakeDateTime);
+        setup();
         fakeDateTime.addDateToReturn(2019,04,20,20,00);
-        order.submit();
         fakeDateTime.addDateToReturn(2019,04,21,21,00);
+        order.submit();
         order.confirm();
+    }
+
+
+    @Test
+    public void shouldBeSubmittedIfOrderIsNotExpired(){
+        setup();
+        fakeDateTime.addDateToReturn(2019,04,20,20,00);
+        fakeDateTime.addDateToReturn(2019,04,20,23,00);
+        order.submit();
+        order.confirm();
+
+        assertThat(order.getOrderState(), Matchers.equalTo(Order.State.SUBMITTED));
+
     }
 }
